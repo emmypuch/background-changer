04b257eae8739e3511503f3f492fef2e1f8d385d
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2021-09-12 23:32:41 +0100

    updated

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 const colorSelector = document.getElementById("colorSelector");
+
+colorSelector.addEventListener("input", function (e) {
+  document.body.style.backgroundColor = colorSelector.value;
+});
